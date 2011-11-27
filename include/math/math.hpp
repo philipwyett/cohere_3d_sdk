70b051b8d8bebdfe95762202fc497ff9a02bef07
Philip Wyett <philip.wyett@cohere-technology.co.uk>
2011-11-27 22:12:54 +0000

    Added version.hpp, math.hpp and math.cpp

---
+++ include/math/math.hpp
@@ -0,0 +1,26 @@
+//
+// Cohere 3D Source Code is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// Cohere 3D Source Code is distributed in the hope that it will be
+// of use, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License version 3 in the 'LICENSE' file provided
+// for details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Cohere 3D Source Code. If not, please
+// see <http://www.gnu.org/licenses/>.
+//
+// If you have questions concerning this license or any additional terms,
+// you may contact the founding developer by email. Please see in the
+// 'CONTACT' file provided for details.
+//
+
+#ifndef __MATH_HPP__
+#define __MATH_HPP__
+
+#endif // End of: __MATH_HPP__
+
